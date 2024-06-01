cf4deafea91a4882ef45e60e6a5aad673a138207
Arya P K <aryapk1913@gmail.com>
2024-06-01 15:43:30 +0530

    reply to the wish

--- model.go
+++ model.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-
-func main(){
+func main() {
 	fmt.Println("hello how are you")
 	fmt.Println("not good")
 
 	fmt.Println("fine")
+	fmt.Println("what about you")
 }
